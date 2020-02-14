2602c42ddb1bd3c2a5db1594ac0765cc54596c52
Miguel Sanchez <miguedep@gmail.com>
2020-02-14 01:16:32 +0100

    function generateHash

--- app.js
+++ app.js
@@ -8,30 +8,34 @@
  */
 
 // Write your code here
+var SHA256 = require('crypto-js/sha256');
 
 /**
  * Variables: Do not change variable values to be able to answer the quiz
  */
 
-const data1 = "Blockchain Rock!";
+const data1 = 'Blockchain Rock!';
 const dataObject = {
-	id: 1,
-  	body: "With Object Works too",
-  	time: new Date().getTime().toString().slice(0,-3)
+    id: 1,
+    body: 'With Object Works too',
+    time: new Date()
+        .getTime()
+        .toString()
+        .slice(0, -3),
 };
 
 /**
  * Step 3: Add code to the `generate hash function
  * Function that generate the SHA256 Hash
- * @param {*} obj 
+ * @param {*} obj
  */
 
 function generateHash(obj) {
-	// Write your code here
+    return SHA256(JSON.stringify(obj));
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
-console.log("************************************");
+console.log('************************************');
 console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
 
 /**
